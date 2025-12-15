fa62e8800c2f53871b0eb36e7bf4e0613aaacf33
ankitkrg9 <ank.1234321kit@gmail.com>
2025-12-15 11:47:23 +0530

    Update API base URL for production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,14 @@
+// import axios from "axios";
+
+// const api = axios.create({
+//   baseURL: "http://localhost:5000/api"
+// });
+
+// export default api;
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://sweet-shop-backend-6em7.onrender.com/api",
 });
 
 export default api;
