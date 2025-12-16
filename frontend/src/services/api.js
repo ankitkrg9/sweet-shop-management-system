2a42316f34b1109243a2b94cc20d9b59dfb3d013
ankitkrg9 <ank.1234321kit@gmail.com>
2025-12-17 03:42:13 +0530

    Prepare frontend for production deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -5,10 +5,17 @@
 // });
 
 // export default api;
+// import axios from "axios";
+
+// const api = axios.create({
+//   baseURL: "https://sweet-shop-backend-6em7.onrender.com/api",
+// });
+
+// export default api;
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://sweet-shop-backend-6em7.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL + "/api",
 });
 
 export default api;
